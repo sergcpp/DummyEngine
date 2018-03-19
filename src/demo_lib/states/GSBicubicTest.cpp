7a578d5181e00cd46a6d6f3b7923d24e651e11a4
Serhii Yablokov <mrapfel1994@gmail.com>
2018-03-19 23:51:19 +0100

    -

--- src/demo_lib/states/GSBicubicTest.cpp
+++ src/demo_lib/states/GSBicubicTest.cpp
@@ -33,22 +33,46 @@ namespace GSBicubicTestInternal {
         return ret;
     }
 
-    image_t UpscaleLinear(const image_t &img) {
+    image_t UpscaleLinear(const image_t &img, int s) {
         image_t ret;
         ret.format = img.format;
-        ret.w = img.w * 2;
-        ret.h = img.h * 2;
+        ret.w = img.w * s;
+        ret.h = img.h * s;
 
         if (ret.format == ren::RawRGB888) {
             ret.data = std::unique_ptr<uint8_t[]>{ new uint8_t[ret.w * ret.h * 3] };
-            for (int j = 0; j < img.h; j++) {
-                for (int i = 0; i < img.w; i++) {
-                    for (int k = 0; k < 3; k++) {
-                        ret.data[3 * ((2 * j + 0) * ret.w + (2 * i + 0)) + k] = img.data[3 * (j * img.w + i) + k];
-                        ret.data[3 * ((2 * j + 0) * ret.w + (2 * i + 1)) + k] = img.data[3 * (j * img.w + i) + k];
-                        ret.data[3 * ((2 * j + 1) * ret.w + (2 * i + 0)) + k] = img.data[3 * (j * img.w + i) + k];
-                        ret.data[3 * ((2 * j + 1) * ret.w + (2 * i + 1)) + k] = img.data[3 * (j * img.w + i) + k];
-                    }
+
+            auto sample = [](const image_t &img, float x, float y, uint8_t *out_col) {
+                int x0 = (int)(x * img.w - 0.5f);
+                int y0 = (int)(y * img.h - 0.5f);
+                int x1 = std::min(x0 + 1, img.w - 1);
+                int y1 = std::min(y0 + 1, img.h - 1);
+
+                float fac_x = (x * img.w - 0.5f) - x0,
+                      fac_y = (y * img.h - 0.5f) - y0;
+
+                x0 = std::max(x0, 0);
+                y0 = std::max(y0, 0);
+
+                for (int i = 0; i < 3; i++) {
+                    float v1 = img.data[3 * (y0 * img.w + x0) + i] * (1 - fac_x) + img.data[3 * (y0 * img.w + x1) + i] * fac_x;
+                    float v2 = img.data[3 * (y1 * img.w + x0) + i] * (1 - fac_x) + img.data[3 * (y1 * img.w + x1) + i] * fac_x;
+
+                    float v = v1 * (1 - fac_y) + v2 * fac_y;
+
+                    out_col[i] = uint8_t(v);
+                }
+            };
+
+            const float off_x = 0.5f / ret.w,
+                        off_y = 0.5f / ret.h;
+
+            for (int j = 0; j < ret.h; j++) {
+                for (int i = 0; i < ret.w; i++) {
+                    float x = off_x + float(i) / ret.w,
+                          y = off_y + float(j) / ret.h;
+
+                    sample(img, x, y, &ret.data[3 * (j * ret.w + i)]);
                 }
             }
         }
@@ -105,25 +129,30 @@ void GSBicubicTest::Draw(float dt_s) {
         glDrawPixels(orig_image_.w, orig_image_.h, GL_RGB, GL_UNSIGNED_BYTE, &orig_image_.data[0]);
         pos_x += float(orig_image_.w * 2)/game_->width;
 
-        image_t new_img1 = UpscaleSimple(orig_image_);
+        image_t new_img1 = UpscaleLinear(orig_image_, 32);
         glRasterPos2f(pos_x, -1);
         glDrawPixels(new_img1.w, new_img1.h, GL_RGB, GL_UNSIGNED_BYTE, &new_img1.data[0]);
         pos_x += float(new_img1.w * 2)/game_->width;
 
-        image_t new_img2 = UpscaleSimple(new_img1);
+        /*image_t new_img2 = UpscaleLinear(new_img1);
         glRasterPos2f(pos_x, -1);
         glDrawPixels(new_img2.w, new_img2.h, GL_RGB, GL_UNSIGNED_BYTE, &new_img2.data[0]);
         pos_x += float(new_img2.w * 2)/game_->width;
 
-        image_t new_img3 = UpscaleSimple(new_img2);
+        image_t new_img3 = UpscaleLinear(new_img2);
         glRasterPos2f(pos_x, -1);
         glDrawPixels(new_img3.w, new_img3.h, GL_RGB, GL_UNSIGNED_BYTE, &new_img3.data[0]);
         pos_x += float(new_img3.w * 2) / game_->width;
 
-        image_t new_img4 = UpscaleSimple(new_img3);
+        image_t new_img4 = UpscaleLinear(new_img3);
         glRasterPos2f(pos_x, -1);
         glDrawPixels(new_img4.w, new_img4.h, GL_RGB, GL_UNSIGNED_BYTE, &new_img4.data[0]);
         pos_x += float(new_img4.w * 2) / game_->width;
+
+        image_t new_img5 = UpscaleLinear(new_img4);
+        glRasterPos2f(pos_x, -1);
+        glDrawPixels(new_img5.w, new_img5.h, GL_RGB, GL_UNSIGNED_BYTE, &new_img5.data[0]);
+        pos_x += float(new_img5.w * 2) / game_->width;*/
     }
 
     {
