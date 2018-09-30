e0cb3ec7be6212f36b5b4fe3e4752f3d307ab44a
Serhii Yablokov <mrapfel1994@gmail.com>
2018-10-01 01:50:53 +0200

    update

--- src/DemoLib/Scene/SceneManager_PT.cpp
+++ src/DemoLib/Scene/SceneManager_PT.cpp
@@ -1,5 +1,7 @@
 #include "SceneManager.h"
 
+#include <map>
+
 #include <Ren/Context.h>
 
 #include "Renderer.h"
@@ -42,11 +44,67 @@ void SceneManager::DrawPT() {
             ray_scene_->AddLight(sun_desc);
         }
 
-        // Add objects
-        for (const auto &o : objects_) {
-            const uint32_t drawable_flags = HasMesh | HasTransform;
-            if ((o.flags & drawable_flags) == drawable_flags) {
+        std::map<std::string, uint32_t> loaded_materials, loaded_meshes;
 
+        uint32_t default_white_tex;
+
+        {
+            Ray::pixel_color8_t white = { 255, 255, 255, 255 };
+
+            Ray::tex_desc_t tex_desc;
+            tex_desc.data = &white;
+            tex_desc.w = tex_desc.h = 1;
+            tex_desc.generate_mipmaps = true;
+
+            default_white_tex = ray_scene_->AddTexture(tex_desc);
+        }
+
+        // Add objects
+        for (const auto &obj : objects_) {
+            const uint32_t drawable_flags = HasMesh | HasTransform;
+            if ((obj.flags & drawable_flags) == drawable_flags) {
+                const auto *mesh = obj.mesh.get();
+                const char *mesh_name = mesh->name();
+
+                auto mesh_it = loaded_meshes.find(mesh_name);
+                if (mesh_it == loaded_meshes.end()) {
+                    Ray::mesh_desc_t mesh_desc;
+                    mesh_desc.prim_type = Ray::TriangleList;
+                    mesh_desc.layout = Ray::PxyzNxyzTuvTuv;
+                    mesh_desc.vtx_attrs = (const float *)mesh->attribs();
+                    mesh_desc.vtx_attrs_count = (uint32_t)(mesh->attribs_size() / (13 * sizeof(float)));
+                    mesh_desc.vtx_indices = (const uint32_t *)mesh->indices();
+                    mesh_desc.vtx_indices_count = (uint32_t)(mesh->indices_size() / sizeof(uint32_t));
+
+                    const Ren::TriStrip *s = &mesh->strip(0);
+                    while (s->offset != -1) {
+                        const auto *mat = s->mat.get();
+                        const char *mat_name = mat->name();
+
+                        auto mat_it = loaded_materials.find(mat_name);
+                        if (mat_it == loaded_materials.end()) {
+                            Ray::mat_desc_t mat_desc;
+                            mat_desc.type = Ray::DiffuseMaterial;
+                            mat_desc.main_texture = default_white_tex;
+                            mat_desc.main_color[0] = mat_desc.main_color[1] = mat_desc.main_color[2] = 0.5f;
+
+                            uint32_t new_mat = ray_scene_->AddMaterial(mat_desc);
+
+                            mat_it = loaded_materials.emplace(mat_name, new_mat).first;
+                        }
+
+                        mesh_desc.shapes.push_back({ mat_it->second, (uint32_t)(s->offset / sizeof(uint32_t)), (uint32_t)s->num_indices });
+
+                        ++s;
+                    }
+
+                    uint32_t new_mesh = ray_scene_->AddMesh(mesh_desc);
+                    mesh_it = loaded_meshes.emplace(mesh_name, new_mesh).first;
+                }
+
+                const auto *tr = obj.tr.get();
+
+                ray_scene_->AddMeshInstance(mesh_it->second, Ren::ValuePtr(tr->mat));
             }
         }
     }
