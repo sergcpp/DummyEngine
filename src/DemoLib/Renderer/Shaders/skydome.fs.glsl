1783d5d317f90468d7f1f69bf096195f9723fc9d
Serhii Yablokov <mrapfel1994@gmail.com>
2019-02-16 13:04:52 +0100

    Set location for output variables

--- src/DemoLib/Renderer/Shaders/skydome.fs.glsl
+++ src/DemoLib/Renderer/Shaders/skydome.fs.glsl
@@ -11,13 +11,14 @@ layout(location = 0) uniform vec3 camera_pos;
 
 in vec3 aVertexPos_;
 
-out vec4 outColor;
-out vec4 outSpecular;
+layout(location = 0) out vec4 outColor;
+layout(location = 1) out vec4 outSpecular;
 
 void main() {
 	vec3 view_dir_ws = normalize(aVertexPos_ - camera_pos);
 
     outColor = texture(env_texture, view_dir_ws);
+outColor.r = 1.0;
 	outSpecular = vec4(0.0, 0.0, 0.0, 1.0);
 }
 )"
