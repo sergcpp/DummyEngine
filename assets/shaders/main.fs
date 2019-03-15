3b82824f578e9318acc9919409c257f3617898a7
Serhii Yablokov <MrApfel1994@gmail.com>
2019-03-15 10:21:35 +0100

    Remove test code

--- assets/shaders/main.fs
+++ assets/shaders/main.fs
@@ -255,8 +255,9 @@ void main(void) {
     vec3 sh_l_12 = texture(lm_indirect_sh_texture[3], lm_uvs).rgb;
     
     //indirect_col += sh_l_00 + sh_l_10 * normal.y + sh_l_11 * normal.z + sh_l_12 * normal.x;
-    indirect_col += sh_l_00;
-    //indirect_col += (0.5 + (sh_l_10 - vec3(0.5)) * normal.y + (sh_l_11 - vec3(0.5)) * normal.z + (sh_l_12 - vec3(0.5)) * normal.x) * sh_l_00 * 2.0;
+    indirect_col += (0.5 + (sh_l_10 - vec3(0.5)) * normal.y +
+                           (sh_l_11 - vec3(0.5)) * normal.z +
+                           (sh_l_12 - vec3(0.5)) * normal.x) * sh_l_00 * 2.0;
     indirect_col = max(indirect_col, vec3(0.0));
     
     vec2 ao_uvs = gl_FragCoord.xy / vec2(float(res.x), float(res.y));
