90f7e30cc82f36c32c724c5026fdc37d558b2683
Serhii Yablokov <MrApfel1994@gmail.com>
2019-05-27 13:12:35 +0200

    Squashed 'src/Sys/' changes from 42f433b..ef8d20e
    ef8d20e Some refactoring
    
    git-subtree-dir: src/Sys
    git-subtree-split: ef8d20e8f881f2eded043c943c89f9d3668dfb0b

--- Time.cpp
+++ Time.cpp
@@ -4,11 +4,9 @@
 
 namespace Sys {
 std::chrono::steady_clock::time_point init_time = std::chrono::steady_clock::now();
-
-unsigned int cached_time = 0;
 }
 
-unsigned int Sys::GetTicks() {
+uint32_t Sys::GetTimeMs() {
     auto t = (std::chrono::steady_clock::now() - init_time);
     auto tt = std::chrono::duration_cast<std::chrono::milliseconds>(t);
     return (unsigned int)tt.count();
