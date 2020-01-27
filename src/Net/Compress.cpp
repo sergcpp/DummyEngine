a029cba65b14e1877691013543fe037b01d00766
Sergey Yablokov <yablokovsergey1@gmail.com>
2020-01-27 14:46:09 +0100

    Extend compression interface

--- src/Net/Compress.cpp
+++ src/Net/Compress.cpp
@@ -27,10 +27,25 @@ Net::Packet Net::CompressLZO(const Packet &pack) {
 Net::Packet Net::DecompressLZO(const Packet &pack) {
     Packet res(4096);
     lzo_uint decompressed_size = res.size();
-    //lzo1x_decompress(&pack[0], pack.size(), &res[0], &decompressed_size, NULL);
-    lzo1x_decompress_safe(&pack[0], pack.size(), &res[0], &decompressed_size, NULL);
+    lzo1x_decompress_safe(&pack[0], pack.size(), &res[0], &decompressed_size, nullptr);
 
     res.resize(decompressed_size);
 
     return res;
+}
+
+int Net::CalcLZOOutSize(const int in_size) {
+    return (in_size + in_size / 16 + 64 + 3);
+}
+
+int Net::CompressLZO(const uint8_t *in_buf, int in_size, uint8_t *out_buf) {
+    lzo_uint compressed_size;
+    lzo1x_1_compress(in_buf, (lzo_uint)in_size, out_buf, &compressed_size, lzo_ctx.working_mem);
+    return (int)compressed_size;
+}
+
+int Net::DecompressLZO(const uint8_t *in_buf, int in_size, uint8_t *out_buf, int out_size) {
+    lzo_uint decompressed_size = out_size;
+    lzo1x_decompress_safe(in_buf, in_size, out_buf, &decompressed_size, nullptr);
+    return (int)decompressed_size;
 }
