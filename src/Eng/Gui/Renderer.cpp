44c3a0e5241d56cbcb621549368ae3970622d1b7
Sergey Yablokov <yablokovsergey1@gmail.com>
2020-04-18 21:05:36 +0200

    Improve line/curve drawing

--- src/Eng/Gui/Renderer.cpp
+++ src/Eng/Gui/Renderer.cpp
@@ -1,4 +1,6 @@
 #include "Renderer.h"
 
 const uint8_t Gui::ColorWhite[4] = {255, 255, 255, 255};
+const uint8_t Gui::ColorGrey[4] = {127, 127, 127, 255};
+const uint8_t Gui::ColorBlack[4] = {0, 0, 0, 255};
 const uint8_t Gui::ColorCyan[4] = {0, 255, 255, 255};
