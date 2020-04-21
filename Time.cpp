2d7476f785b1011d43a673ee76d42101e440e332
Sergey Yablokov <yablokovsergey1@gmail.com>
2020-04-21 10:22:01 +0200

    Small fix

--- Time.cpp
+++ Time.cpp
@@ -9,7 +9,7 @@ std::chrono::steady_clock::time_point init_time = std::chrono::steady_clock::now
 uint32_t Sys::GetTimeMs() {
     auto t = (std::chrono::steady_clock::now() - init_time);
     auto tt = std::chrono::duration_cast<std::chrono::milliseconds>(t);
-    return (unsigned int)tt.count();
+    return (uint32_t)tt.count();
 }
 
 uint64_t Sys::GetTimeUs() {
