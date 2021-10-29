5ef9b1342c8ff3ed703ff77d3b6f2d1bc07e4b56
Sergey Yablokov <yablokovsergey1@gmail.com>
2021-10-29 09:21:00 +0200

    Fix non-unity build

--- src/Eng/Renderer/Passes/RpDepthHierarchy.h
+++ src/Eng/Renderer/Passes/RpDepthHierarchy.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <Ren/Pipeline.h>
+
 #include "../Graph/GraphBuilder.h"
 
 struct ViewState;
--- src/Eng/Renderer/Passes/RpSkinning.cpp
+++ src/Eng/Renderer/Passes/RpSkinning.cpp
@@ -1,5 +1,7 @@
 #include "RpSkinning.h"
 
+#include <Ren/Context.h>
+
 #include "../../Utils/ShaderLoader.h"
 #include "../Renderer_Structs.h"
 
