386524a6ae78c11f9a57e574d1930c8d491a3095
Sergey Yablokov <yablokovsergey1@gmail.com>
2022-03-31 02:49:22 +0200

    Add inplace function class

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,7 @@
 
 void test_alloc();
 void test_async_file();
+void test_inplace_function();
 void test_json();
 void test_optional();
 void test_pack();
@@ -12,6 +13,7 @@ void test_vector();
 int main() {
     test_alloc();
     test_async_file();
+    test_inplace_function();
     test_json();
     test_optional();
     //test_pack();
