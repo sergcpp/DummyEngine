f0166e2ee46d82643095b6e00840092150f9b9dd
Sergey Yablokov <yablokovsergey1@gmail.com>
2022-06-30 04:35:04 +0200

    Remove TODOs

--- src/DummyApp/main_pc.cpp
+++ src/DummyApp/main_pc.cpp
@@ -5,23 +5,3 @@ int main(int argc, char *argv[]) {
     return DummyApp().Run(argc, argv);
 }
 
-// TODO:
-// decal masks
-// fix exposure flicker
-// use texture array for lightmaps
-// texture streaming
-// use stencil to distinguich ssr/nossr regions <-- msaa :(
-// use GL_EXT_shader_group_vote
-// refactor msaa (resolve once, remove permutations)
-// refactor file read on android
-// start with scene editing
-// add assetstream
-// get rid of SDL in Modl app
-// make full screen quad passes differently
-// refactor repetitive things in shaders
-// use frame graph approach in renderer
-// check GL_QCOM_alpha_test extension (for depth prepass and shadow rendering)
-// check GL_QCOM_tiled_rendering extension
-// try to use texture views to share framebuffers texture memory (GL_OES_texture_view)
-// use one big array for instance indices
-// get rid of SOIL in Ren (??? png loading left)
