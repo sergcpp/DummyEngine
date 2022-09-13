be30ad09ad5611601956ce12357074ebc83b42ca
Sergey Yablokov <yablokovsergey1@gmail.com>
2022-09-13 03:07:14 +0200

    --

--- src/Eng/Renderer/Passes/RpDebugRT.cpp
+++ src/Eng/Renderer/Passes/RpDebugRT.cpp
@@ -7,3 +7,13 @@
 #include "../../Utils/ShaderLoader.h"
 #include "../PrimDraw.h"
 #include "../Renderer_Structs.h"
+
+void RpDebugRT::Execute(RpBuilder &builder) {
+    if (builder.ctx().capabilities.raytracing) {
+        Execute_HWRT(builder);
+    } else {
+        Execute_SWRT(builder);
+    }
+}
+
+void RpDebugRT::Execute_SWRT(RpBuilder &builder) { LazyInit(builder.ctx(), builder.sh()); }
