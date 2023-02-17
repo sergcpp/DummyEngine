e4695286c8ae761c510e85c0b8f83fa2d773d03f
Sergey Yablokov <yablokovsergey1@gmail.com>
2023-02-17 04:06:25 +0100

    Remove Platform class

--- Platform.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#if defined(_WIN32) || defined(__linux__) || defined(__EMSCRIPTEN__)
-#include "PlatformSDL.h"
-#endif
--- _Sys.cpp
+++ _Sys.cpp
@@ -15,9 +15,5 @@
 #include "AsyncFileReader_posix_aio.cpp"
 #endif
 
-#if defined(_WIN32) || defined(__linux__) || defined(__EMSCRIPTEN__)
-#include "PlatformSDL.cpp"
-#endif
-
 // includes Windows.h with all macro crap, so should be last
 #include "DynLib.cpp"
