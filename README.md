487089203334560fb313ad32102b9680b54e3306
Sergey Yablokov <yablokovsergey1@gmail.com>
2024-05-23 19:00:57 +0200

    New sponza scene

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Cross platform Vulkan/OpenGL 3d engine for personal experimentation.
 
 ![](ai043_06.png)
+![](sponza.png)
--- assets/scenes_src/export.py
+++ assets/scenes_src/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6f4bb6ce2e5e87905f90cafff676f463c2d5db30dcb399ca8213ef44bd55146
-size 25607
+oid sha256:7373ff6fcf897a505f408de8d072487b477fb9f355ccdb43198ce1b5d1af0023
+size 26930
