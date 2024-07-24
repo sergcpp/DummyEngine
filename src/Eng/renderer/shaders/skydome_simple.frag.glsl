5db13f3060c0c4511d2bf29472378302968d3250
Sergey Yablokov <yablokovsergey1@gmail.com>
2024-07-24 14:38:53 +0200

    Include sun disk to medium preset sky

--- src/Eng/renderer/shaders/skydome_simple.frag.glsl
+++ src/Eng/renderer/shaders/skydome_simple.frag.glsl
@@ -11,6 +11,8 @@ LAYOUT_PARAMS uniform UniformParams {
     Params g_params;
 };
 
+#define SKY_SUN_BLEND_VAL 0.0005
+
 layout(binding = ENV_TEX_SLOT) uniform samplerCube g_env_tex;
 
 layout(location = 0) in vec3 g_vtx_pos;
@@ -21,6 +23,15 @@ void main() {
     const vec3 view_dir_ws = normalize(g_vtx_pos);
 
     const vec3 rotated_dir = rotate_xz(view_dir_ws, g_shrd_data.env_col.w);
-    g_out_color.rgb = compress_hdr(g_shrd_data.env_col.xyz * texture(g_env_tex, rotated_dir).rgb);
-    g_out_color.a = 1.0;
+    vec3 out_color = g_shrd_data.env_col.xyz * texture(g_env_tex, rotated_dir).xyz;
+
+    // sun disk is missing from cubemap, add it here
+    if (g_shrd_data.sun_dir.w > 0.0 && view_dir_ws.y > -0.01) {
+        const float costh = dot(view_dir_ws, g_shrd_data.sun_dir.xyz);
+        const float cos_theta = 1.0 / sqrt(1.0 + g_shrd_data.sun_dir.w * g_shrd_data.sun_dir.w);
+        const float sun_disk = smoothstep(cos_theta - SKY_SUN_BLEND_VAL, cos_theta + SKY_SUN_BLEND_VAL, costh);
+        out_color += sun_disk * g_shrd_data.sun_col.xyz;
+    }
+
+    g_out_color = vec4(compress_hdr(out_color), 1.0);
 }
