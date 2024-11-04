835ea770632ee4cb82867248fa29601b4aec1432
Sergey Yablokov <yablokovsergey1@gmail.com>
2024-11-05 00:46:45 +0100

    Minor refactoring

--- src/Eng/scene/components/Transform.h
+++ src/Eng/scene/components/Transform.h
@@ -9,6 +9,7 @@
 namespace Eng {
 struct Transform {
     // streaming data
+    Ren::Vec3f euler_angles_rad, scale;
 
     // temporary data
     Ren::Mat4f world_from_object, object_from_world, world_from_object_prev;
@@ -16,7 +17,6 @@ struct Transform {
     uint32_t node_index = 0xffffffff;
     Ren::Vec3f bbox_max;
     Ren::Vec3f bbox_min_ws, bbox_max_ws;
-    Ren::Vec3f euler_angles_rad, scale;
 
     void UpdateBBox();
     void UpdateInvMatrix();
