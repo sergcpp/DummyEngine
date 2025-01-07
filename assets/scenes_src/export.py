d2b300cfea8cac948de30982c207dd7c7aeb1240
Sergey Yablokov <yablokovsergey1@gmail.com>
2025-01-07 12:42:32 +0100

    Bathroom scene

--- assets/scenes_src/export.py
+++ assets/scenes_src/export.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3985628b4a0a8c4cd747afcf9eecf077b2f97581339f1678ed17edb8835643e6
-size 31044
+oid sha256:00e03aa17c97727552979f8ae2ab96c14a2f80b98561eb5278d3845bd5f15dc0
+size 31144
