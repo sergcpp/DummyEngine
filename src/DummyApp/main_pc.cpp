227ce1b98f611bfa3a98e8de77a5c2a2b117e00f
Serhii Yablokov <serhii.yablokov@ubisoft.com>
2025-05-07 17:56:52 +0200

    +++

--- src/DummyApp/main_pc.cpp
+++ src/DummyApp/main_pc.cpp
@@ -1,53 +1,217 @@
 #include "DummyApp.h"
 
 #include <fstream>
+#include <random>
 
-#include <Ren/Span.h>
 #include <Ray/internal/PMJ.h>
+#include <Ren/Span.h>
 
-double test_function(double x, double min, double max) {
+const int SampleCount = 256; // Must be power of 2
+const int TileRes = 128;
+const int TestIntegralsCount = 1000;
+
+float test_function(float x, float min, float max) {
     if (x > min && x < max) {
         return 1.0f;
     }
     return 0.0f;
 }
 
-double integral_val(double min, double max) { return max - min; }
+float integral_val(float min, float max) { return max - min; }
 
-double evaluate_integrals(const double samples[256]) {
+float evaluate_integrals(const float samples[], const int sample_count) {
     double total_error = 0.0;
-    for (int i = 0; i < 256; ++i) {
-        const double min_val = double(i) / 255.0;
-        for (int j = i; j < 256; ++j) {
-            const double max_val = double(j) / 255.0;
+    for (int j = 0; j < TestIntegralsCount; ++j) {
+        const float point = float(j) / float(TestIntegralsCount - 1);
 
-            double result = 0.0;
-            for (int k = 0; k < 256; ++k) {
-                result += test_function(samples[k], min_val, max_val);
-            }
-            result /= 256.0;
-
-            total_error += std::abs(result - integral_val(min_val, max_val));
+        float results[2] = {};
+        for (int k = 0; k < sample_count; ++k) {
+            const float f = test_function(samples[k], 0.0f - 0.1f, point);
+            // white region first, black region second
+            results[0] += f;
+            // black region first, white region second
+            results[1] += 1.0f - f;
         }
+        results[0] /= sample_count;
+        results[1] /= sample_count;
+
+        const float integral = integral_val(0.0f, point);
+        total_error += std::abs(results[0] - integral) + std::abs(results[1] - 1.0f + integral);
     }
-    return total_error;
+    return float(total_error / TestIntegralsCount);
 }
 
-void WriteTGA(const double *data, int pitch, const int w, const int h, const int bpp, const char *name);
+float calc_pixel_proximity(const int ox, const int oy, const float values[TileRes][TileRes]) {
+    const float v = values[oy][ox];
+
+    // TODO: Is this really OK to limit the radius?
+    const int Radius = 4;
+
+    float total_proximity = 0.0f;
+    for (int y = oy - Radius; y <= oy + Radius; ++y) {
+        const int wrapped_y = (y + TileRes) % TileRes;
+        for (int x = ox - Radius; x <= ox + Radius; ++x) {
+            const int wrapped_x = (x + TileRes) % TileRes;
+            if (wrapped_x == ox && wrapped_y == oy) {
+                continue;
+            }
+
+            const float GaussOmega = 2.1f;
+
+            total_proximity += std::exp(-((x - ox) * (x - ox) + (y - oy) * (y - oy)) / GaussOmega) *
+                               (values[wrapped_y][wrapped_x] - v) * (values[wrapped_y][wrapped_x] - v);
+        }
+    }
+
+    return total_proximity;
+}
+
+uint32_t hash(uint32_t x) {
+    // finalizer from murmurhash3
+    x ^= x >> 16;
+    x *= 0x85ebca6bu;
+    x ^= x >> 13;
+    x *= 0xc2b2ae35u;
+    x ^= x >> 16;
+    return x;
+}
+
+uint32_t hash_combine(uint32_t seed, uint32_t v) { return seed ^ (v + (seed << 6u) + (seed >> 2u)); }
+
+void WriteTGA(const float *data, int pitch, int w, int h, int px_stride, int bpp, const char *name);
 
 #undef main
 int main(int argc, char *argv[]) {
 #if 1
-    Ray::aligned_vector<Ray::Ref::dvec2> pmj_samples = Ray::GeneratePMJSamples(0, 256, 1);
+    Ray::aligned_vector<Ray::Ref::dvec2> pmj_samples = Ray::GeneratePMJSamples(123456, SampleCount, 1);
 
-    double samples[256];
-    for (int i = 0; i < 256; ++i) {
-        samples[i] = pmj_samples[i].get<0>();
+    uint32_t initial_samples[SampleCount];
+    for (int i = 0; i < SampleCount; ++i) {
+        initial_samples[i] = uint32_t(pmj_samples[i].get<0>() * 16777216.0) << 8;
     }
 
-    double error = evaluate_integrals(samples);
+    uint32_t scrambling_keys[TileRes][TileRes];
+    for (int y = 0; y < TileRes; ++y) {
+        for (int x = 0; x < TileRes; ++x) {
+            scrambling_keys[y][x] = hash(y * TileRes + x) & 0xffffff00;
+        }
+    }
 
-    WriteTGA(samples, 256, 256, 256, 3, "test.tga");
+    static float samples[TileRes][TileRes][SampleCount];
+    for (int y = 0; y < TileRes; ++y) {
+        for (int x = 0; x < TileRes; ++x) {
+            for (int i = 0; i < SampleCount; ++i) {
+                const uint32_t scrambled_val = initial_samples[i] ^ scrambling_keys[y][x];
+                samples[y][x][i] = float(scrambled_val >> 8) / 16777216.0f;
+            }
+        }
+    }
+
+    float min_error = FLT_MAX, max_error = 0.0f;
+
+    float errors[TileRes][TileRes];
+    for (int y = 0; y < TileRes; ++y) {
+        for (int x = 0; x < TileRes; ++x) {
+            errors[y][x] = evaluate_integrals(samples[y][x], 256);
+            min_error = std::min(min_error, errors[y][x]);
+            max_error = std::max(max_error, errors[y][x]);
+        }
+    }
+    // normalize (for easier debugging)
+    for (int i = 0; i < TileRes * TileRes; ++i) {
+        float &e = errors[i / TileRes][i % TileRes];
+        e = (e - min_error) / (max_error - min_error);
+    }
+
+    const int AnnealingIterations = 100000;
+
+    float best_proximity = 0.0f, last_proximity = 0.0f;
+
+    std::mt19937 gen(123456);
+    std::uniform_int_distribution<int> uniform_index(0, TileRes * TileRes - 1);
+    std::uniform_real_distribution<float> uniform_unorm_float(0.0f, 1.0f);
+
+    float T = 1.0f, T_min = 0.0001f;
+    for (int iter = 0; iter < AnnealingIterations; ++iter) {
+        printf("Annealing Iteration %i\n", iter);
+
+        // Randomly swap two scrambling keys
+        int index1 = uniform_index(gen), index2;
+        do {
+            index2 = uniform_index(gen);
+        } while (index1 == index2);
+
+        std::swap(scrambling_keys[index1 / TileRes][index1 % TileRes],
+                  scrambling_keys[index2 / TileRes][index2 % TileRes]);
+        for (int i = 0; i < SampleCount; ++i) {
+            std::swap(samples[index1 / TileRes][index1 % TileRes][i], samples[index2 / TileRes][index2 % TileRes][i]);
+        }
+        std::swap(errors[index1 / TileRes][index1 % TileRes], errors[index2 / TileRes][index2 % TileRes]);
+
+        char temp_buf[128];
+        // snprintf(temp_buf, sizeof(temp_buf), "samples_%i.tga", iter);
+        // WriteTGA(&samples[0][0], TileRes, TileRes, TileRes, 3, temp_buf);
+
+        float proximity[TileRes][TileRes];
+        float total_proximity = 0.0f;
+        for (int y = 0; y < TileRes; ++y) {
+            for (int x = 0; x < TileRes; ++x) {
+                const float prox = calc_pixel_proximity(x, y, errors);
+                proximity[y][x] = prox;
+                total_proximity += prox;
+            }
+        }
+
+        // snprintf(temp_buf, sizeof(temp_buf), "proximity_%i.tga", iter);
+        // WriteTGA(&proximity[0][0], TileRes, TileRes, TileRes, 3, temp_buf);
+
+        // We still accept worse permutation sometimes to avoid being stuck in local minimum
+        const float acceptance_prob = std::exp((total_proximity - last_proximity) / T);
+        // if (total_proximity < last_proximity) {
+        // printf("acceptance %f\n", acceptance_prob);
+        //}
+
+        if (total_proximity > last_proximity || uniform_unorm_float(gen) < acceptance_prob) {
+            // Accept this iteration
+            last_proximity = total_proximity;
+            if (total_proximity > best_proximity) {
+                best_proximity = total_proximity;
+                printf("Best proximity = %f\n", best_proximity);
+
+                // snprintf(temp_buf, sizeof(temp_buf), "best_samples_%i.tga", iter);
+                WriteTGA(&samples[0][0][0], TileRes, TileRes, TileRes, SampleCount, 3, "best_samples.tga");
+
+                WriteTGA(&errors[0][0], TileRes, TileRes, TileRes, 1, 3, "best_errors.tga");
+
+                // snprintf(temp_buf, sizeof(temp_buf), "best_proximity_%i.tga", iter);
+                WriteTGA(&proximity[0][0], TileRes, TileRes, TileRes, 1, 3, "best_proximity.tga");
+            }
+        } else {
+            // Discard this iteration (swap values back)
+            std::swap(scrambling_keys[index1 / TileRes][index1 % TileRes],
+                      scrambling_keys[index2 / TileRes][index2 % TileRes]);
+            for (int i = 0; i < SampleCount; ++i) {
+                std::swap(samples[index1 / TileRes][index1 % TileRes][i],
+                          samples[index2 / TileRes][index2 % TileRes][i]);
+            }
+            std::swap(errors[index1 / TileRes][index1 % TileRes], errors[index2 / TileRes][index2 % TileRes]);
+        }
+
+        T = std::max(T * 0.9999f, T_min);
+    }
+
+    // At this point we have our scrambling keys optimized for SampleCount samples taken,
+    // now we need to find ranking keys which make it work for any power of 2 samples subset
+    uint32_t ranking_keys[TileRes][TileRes] = {};
+
+    int samples_subset = SampleCount / 2;
+    while (samples_subset) {
+        for (int y = 0; y < TileRes; ++y) {
+            for (int x = 0; x < TileRes; ++x) {
+            }
+        }
+        samples_subset /= 2;
+    }
 
     return 0;
 #else
@@ -55,14 +219,10 @@ int main(int argc, char *argv[]) {
 #endif
 }
 
-#define double_to_byte(val)                                                                                             \
+#define float_to_byte(val)                                                                                             \
     (((val) <= 0.0f) ? 0 : (((val) > (1.0f - 0.5f / 255.0f)) ? 255 : uint8_t((255.0f * (val)) + 0.5f)))
 
-void WriteTGA(const double *data, int pitch, const int w, const int h, const int bpp, const char *name) {
-    if (!pitch) {
-        pitch = w;
-    }
-
+void WriteTGA(const float *data, int pitch, const int w, const int h, int px_stride, const int bpp, const char *name) {
     std::ofstream file(name, std::ios::binary);
 
     unsigned char header[18] = {0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -79,11 +239,11 @@ void WriteTGA(const double *data, int pitch, const int w, const int h, const int
     auto out_data = std::unique_ptr<uint8_t[]>{new uint8_t[size_t(w) * h * bpp]};
     for (int j = 0; j < h; ++j) {
         for (int i = 0; i < w; ++i) {
-            out_data[(j * w + i) * bpp + 0] = double_to_byte(data[j * pitch + i]);
-            out_data[(j * w + i) * bpp + 1] = double_to_byte(data[j * pitch + i]);
-            out_data[(j * w + i) * bpp + 2] = double_to_byte(data[j * pitch + i]);
+            out_data[(j * w + i) * bpp + 0] = float_to_byte(data[px_stride * (j * pitch + i)]);
+            out_data[(j * w + i) * bpp + 1] = float_to_byte(data[px_stride * (j * pitch + i)]);
+            out_data[(j * w + i) * bpp + 2] = float_to_byte(data[px_stride * (j * pitch + i)]);
             if (bpp == 4) {
-                out_data[i * 4 + 3] = double_to_byte(data[j * pitch + i]);
+                out_data[i * 4 + 3] = float_to_byte(data[px_stride * (j * pitch + i)]);
             }
         }
     }
